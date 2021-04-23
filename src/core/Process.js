f62e9850a3e78e5911633d1fe7adc9295124c32d
Gabriel Araújo <gabrielaraujogpa@gmail.com>
2021-04-22 21:27:13 -0300

    Fix: left zero adjustment

--- src/core/Process.js
+++ src/core/Process.js
@@ -61,7 +61,9 @@ function Process() {
         min = Math.ceil(min);
         max = Math.floor(max);
     
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+        let result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        return result < 10 ? `0${result}` : result;
     }
 
     function subscribe(observerFunction) {
